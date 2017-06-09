2224bd212f991c566a040a33d06cb59aba651cc1
RomArt <njuvastik@mail.ru>
2017-06-09 16:02:00 +0400

    Исправление при работе с таймером

---
+++ HWT_04/Task03/Program.cs
@@ -0,0 +1,41 @@
+﻿// Проведите сравнительный анализ скорости работы классов String и StringBuilder для операции сложения строк.
+namespace Task11
+{
+    using System;
+    using System.Diagnostics;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            string str = "";
+            StringBuilder sb = new StringBuilder();
+            Stopwatch sw = new Stopwatch();
+            int n = 100;
+            sw.Start();
+            for (int i = 0; i < n; i++)
+            {
+                str += "*";
+            }
+
+            sw.Stop();
+            Console.WriteLine("Время, затраченное на преобразование string в секундах={0}", sw.Elapsed);
+
+            sw.Reset();
+            sw.Start();
+            for (int i = 0; i < n; i++)
+            {
+                sb.Append("*");
+            }
+
+            sw.Stop();
+            Console.WriteLine("Время, затраченное на преобразование stringBuilder в секундах={0}", sw.Elapsed);
+
+            Console.ReadKey();
+        }
+    }
+}
